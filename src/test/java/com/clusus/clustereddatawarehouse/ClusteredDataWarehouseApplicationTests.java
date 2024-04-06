1ef3777d3af38cc0bb8ae975480289e12fe4133d
sijanghm <sijan.ghimire29@gmail.com>
2024-04-06 09:54:05 +0545

    project initilize

---
+++ src/test/java/com/clusus/clustereddatawarehouse/ClusteredDataWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clusus.clustereddatawarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusteredDataWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
